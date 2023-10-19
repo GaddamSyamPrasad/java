6f98630771143e9a4c5c11096e2039f291ae9f82
GaddamSyamPrasad <143477810+GaddamSyamPrasad@users.noreply.github.com>
2023-10-19 15:13:15 +0530

    Create JavaMethodDemo.java

---
+++ JavaMethodDemo.java
@@ -0,0 +1,14 @@
+import java.io.*;
+public class JavaMathDemo{
+    public static void main(String args[]){
+        double x=28;
+        double y=4;
+        System.out.println("Maximum of x and y is "+Math.max(x,y));
+        System.out.println("Minimum of x and y is "+Math.min(x,y));
+        System.out.println("SquareRoot of y is "+Math.sqrt(y));
+        System.out.println("Power of x and y is "+Math.pow(x,y));
+        System.out.println("Logrithum of x is "+Math.log(x));
+        System.out.println("Round of 4.56 "+Math.round(4.56));
+        System.out.println("Log 10 of x is "+Math.log10(x));
+    }
+}
