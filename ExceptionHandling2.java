0f6fabe096022c9e668a69627169fa509ca2110f
GaddamSyamPrasad <143477810+GaddamSyamPrasad@users.noreply.github.com>
2023-11-01 07:50:52 -0700

    Create ExceptionHandling2.java

---
+++ ExceptionHandling2.java
@@ -0,0 +1,14 @@
+public class ExceptionHandling2{
+    public static void main(String[] args) {
+        try {
+            // code that generates exception
+            int divideByZero = 5 / 0;
+        }
+        catch (ArithmeticException e) {
+            System.out.println("ArithmeticException => " + e.getMessage());
+        }
+        finally {
+            System.out.println("This is the finally block");
+        }
+    }
+}
